9201ab371e085b3a2e39214b06e04b0231d6bb8f
Martin Giger <martin@humanoids.be>
2023-04-27 00:11:36 +0200

    chore: update eslint presets

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
- * @author Martin Giger
  * @license MIT
+ * @author Martin Giger
  */
 import path from "node:path";
 import fs from "node:fs";
@@ -16,74 +16,79 @@ import memoize from "lodash.memoize";
 import defaultBasePath from "app-root-path";
 
 /**
- * @class
- * @param {string} [basePath=require("app-root-path")] - Path the transifex configuration is in. Defaults
- *                              to the best guess of the package root.
- * @throws The .transifexrc or .tx/config can not be found.
- * @exports transifex-config
- */
-function TransifexConfig(basePath = defaultBasePath) {
-    /**
-     * Base path the config is read from.
-     *
-     * @type {string}
-     */
-    this.basePath = basePath;
-
-    const R_OK = fs.R_OK || fs.constants.R_OK;
-    /* eslint-disable node/no-sync */
-    fs.accessSync(path.join(this.basePath, TRANSIFEXRC), R_OK);
-    fs.accessSync(path.join(this.basePath, TXCONFIG), R_OK);
-    /* eslint-enable node/no-sync */
-}
-
-/**
- * @typedef {object.<string, string>} ConfigSection
+ * @typedef {Record<string, string>} ConfigSection
  * Has a property for each key in the section, with trimmed name and value.
  */
 /**
- * @typedef {object.<string, module:transifex-config~ConfigSection>} ParsedConfig
+ * @typedef {Record<string, module:transifex-config~ConfigSection>} ParsedConfig
  * Has a property for each section in the config. Each section has a property
  * named after the key with its value assigned.
  */
-/**
- * @async
- * @returns {module:transifex-config~ParsedConfig} Parsed .tx/config as an
- *          object. Will be cached.
- * @throws The config could not be read.
- */
-TransifexConfig.prototype.getConfig = function() {
-    if(!this._txconfig) {
-        this._txconfig = txconfig(this.basePath);
-    }
-    return this._txconfig;
-};
 
 /**
- * @async
  * @param {string} [service] - The config should contain this service URL.
  * @returns {module:transifex-config~ParsedConfig} Parsed .transifexrc as an
  *          object. Will be cached.
+ * @async
  * @throws The .transifexrc could not be read.
- * @this TransifexConfig
+ * @this {TransifexConfig}
  */
 function _getRC(service) {
     return transifexrc(this.basePath, service);
 }
-/**
- * Memoized version of {@link module:transifex-config~_getRC}.
- *
- * @extends module:transifex-config~_getRC
- */
-TransifexConfig.prototype.getRC = memoize(_getRC);
 
 /**
- * @async
- * @returns {Array.<module:transifex-config~ConfigSection>} Array of resources.
- * @throws The config could not be read.
+ * @exports transifex-config
+ * @class
  */
-TransifexConfig.prototype.getResources = function() {
-    return this.getConfig().then((config) => {
+class TransifexConfig {
+    /**
+     * @param {string} [basePath=require("app-root-path")] - Path the transifex configuration is in. Defaults
+     *                              to the best guess of the package root.
+     * @throws The .transifexrc or .tx/config can not be found.
+     */
+    constructor(basePath = defaultBasePath) {
+        /**
+         * Base path the config is read from.
+         *
+         * @type {string}
+         */
+        this.basePath = basePath;
+
+        const R_OK = fs.R_OK || fs.constants.R_OK;
+        /* eslint-disable node/no-sync */
+        fs.accessSync(path.join(this.basePath, TRANSIFEXRC), R_OK);
+        fs.accessSync(path.join(this.basePath, TXCONFIG), R_OK);
+        /* eslint-enable node/no-sync */
+    }
+
+    /**
+     * Memoized version of {@link module:transifex-config~_getRC}.
+     *
+     * @extends module:transifex-config~_getRC
+     */
+    getRC = memoize(_getRC);
+
+    /**
+     * @returns {module:transifex-config~ParsedConfig} Parsed .tx/config as an
+     *          object. Will be cached.
+     * @async
+     * @throws The config could not be read.
+     */
+    getConfig() {
+        if(!this._txconfig) {
+            this._txconfig = txconfig(this.basePath);
+        }
+        return this._txconfig;
+    }
+
+    /**
+     * @returns {Array.<module:transifex-config~ConfigSection>} Array of resources.
+     * @async
+     * @throws The config could not be read.
+     */
+    async getResources() {
+        const config = await this.getConfig();
         const resources = [];
         for(const [
             organization,
@@ -108,24 +113,23 @@ TransifexConfig.prototype.getResources = function() {
             }
         }
         return resources;
-    });
-};
+    }
 
-/**
- * @param {string} localPath - Absolute local path of the resource to return the
- *                             config entry of.
- * @param {boolean} [matchSourceLang] - If the resource should be returned
- *                                            when the path is for the source
- *                                            language of the resource.
- * @async
- * @returns {module:transifex-config~ConfigSection} Config section for the
- *          resource.
- * @throws {module:transifex-config/lib/errors.NoMatchingResourceError} There
- *         is no matching resource.
- * @throws The config could not be read.
- */
-TransifexConfig.prototype.getResource = function(localPath, matchSourceLang) {
-    return this.getResources().then((resources) => {
+    /**
+     * @param {string} localPath - Absolute local path of the resource to return the
+     *                             config entry of.
+     * @param {boolean} [matchSourceLang] - If the resource should be returned
+     *                                            when the path is for the source
+     *                                            language of the resource.
+     * @returns {module:transifex-config~ConfigSection} Config section for the
+     *          resource.
+     * @async
+     * @throws {module:transifex-config/lib/errors.NoMatchingResourceError} There
+     *         is no matching resource.
+     * @throws The config could not be read.
+     */
+    async getResource(localPath, matchSourceLang) {
+        const resources = await this.getResources();
         let lang;
         const resource = resources.find((txResource) => {
             const result = matchResource(this.basePath, localPath, txResource);
@@ -144,41 +148,40 @@ TransifexConfig.prototype.getResource = function(localPath, matchSourceLang) {
         resource.lang = lang;
         resource.source = lang === resource.source_lang;
         return resource;
-    });
-};
+    }
 
-/**
- * Check if a resource is the source resource.
- *
- * @param {string} resourcePath - Path to check.
- * @async
- * @returns {boolean} If the resource is the source.
- * @throws The config could not be read.
- */
-TransifexConfig.prototype.isSourceResource = function(resourcePath) {
-    return this.getResources()
-        .then((resources) => resources.some((resource) => matchResource(this.basePath, resourcePath, resource) == resource.source_lang));
-};
+    /**
+     * Check if a resource is the source resource.
+     *
+     * @param {string} resourcePath - Path to check.
+     * @returns {boolean} If the resource is the source.
+     * @async
+     * @throws The config could not be read.
+     */
+    async isSourceResource(resourcePath) {
+        const resources = await this.getResources();
+        return resources.some((resource) => matchResource(this.basePath, resourcePath, resource) == resource.source_lang);
+    }
 
-/**
- * @param {string} lang - Language code to map from local to external.
- * @param {module:transifex-config~ConfigSection} resource - Resource to get map
- *                                                           the language for.
- * @async
- * @returns {string} Mapped language code.
- * @throws The config could not be read.
- */
-TransifexConfig.prototype.getMappedLang = function(lang, resource) {
-    return this.getConfig().then((globalConfig) => {
+    /**
+     * @param {string} lang - Language code to map from local to external.
+     * @param {module:transifex-config~ConfigSection} resource - Resource to get map
+     *                                                           the language for.
+     * @returns {string} Mapped language code.
+     * @async
+     * @throws The config could not be read.
+     */
+    async getMappedLang(lang, resource) {
+        const globalConfig = await this.getConfig();
         if(!resource.lang_map && !globalConfig.main.lang_map) {
             return lang;
         }
         const map = parseLangMap(resource.lang_map, parseLangMap(globalConfig.main.lang_map));
-        if(lang in map) {
+        if(Object.hasOwn(lang, map)) {
             return map[lang];
         }
         return lang;
-    });
-};
+    }
+}
 
 export default TransifexConfig;
